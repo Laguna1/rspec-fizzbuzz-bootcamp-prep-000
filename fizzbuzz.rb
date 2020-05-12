e4cb07f50070c71e1bd6109f8cf201e805fb888a
Oksana Petrova <lagunazoomarket@gmail.com>
2020-05-12 22:08:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,24 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+# def fizzbuzz(number)
+#   if number % 3 == 0
+#         return "Fizz"
+#     elsif number % 5 == 0
+#         return "Buzz"
+#     elsif number % 15 == 0
+#         return "FizzBuzz"
+#     else
+#         puts "nil"
+#     end
+# end
 def fizzbuzz(number)
-  if number % 3 == 0
-        return "Fizz"
+    if number % 15 == 0
+      "FizzBuzz"
+    elsif number % 3 == 0
+      "Fizz"
     elsif number % 5 == 0
-        return "Buzz"
-    elsif number % 15 == 0
-        return "FizzBuzz"
+      "Buzz"
     else
-        puts "nil"
+      number
     end
-end
-fizzbuzz(15)
+  end
